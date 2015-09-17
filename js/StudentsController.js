099031bdf1998bf740d2168f6d83fed874021f7c
marvinick <marviluw@gmail.com>
2015-09-17 15:27:07 -0700

    add and delete student

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,9 +1,11 @@
-studentRoster.controller('StudentsCtrl', function Ctrl($scope) {
-  $scope.students = [
-    { name: "Sam Schmidt" },
-    { name: "Jessica Martin" },
-    { name: "Sandy Smith" },
-    { name: "Ryan Samuels" },
-    { name: "Brentwood Davis" }
-  ]
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [];
+  $scope.addStudent = function() {
+    $scope.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  };
+  $scope.deleteStudent = function(student) {
+    var index = $scope.students.indexOf(student);
+    $scope.students.splice(index, 1);
+  };
 });
