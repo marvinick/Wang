e436252a933508ec29ed860ac0e08b870f0a3240
marvinick <marviluw@gmail.com>
2015-09-17 15:10:01 -0700

    1st

---
+++ app.js
@@ -0,0 +1,1 @@
+ var studentRoster = angular.module('studentRoster', []);
---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentsCtrl', function Ctrl($scope) {
+  $scope.students = [
+    { name: "Sam Schmidt" },
+    { name: "Jessica Martin" },
+    { name: "Sandy Smith" },
+    { name: "Ryan Samuels" },
+    { name: "Brentwood Davis" }
+  ]
+});
